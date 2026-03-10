56e5c12abfba26800df072fdaca5b70c94feb6e5
Lara <xyuan93@gatech.edu>
2026-03-09 17:42:59 -0700

    component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return <p>Welcome To ProShop</p>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
