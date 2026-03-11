f2172dd04bb4670a24d5119497146343572c3978
Lara <xyuan93@gatech.edu>
2026-03-11 11:46:34 -0700

    error handler middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,6 +9,7 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+    // throw new Error("Test error handling");
     res.json(products);
   }),
 );
@@ -19,7 +20,8 @@ router.get(
     if (product) {
       return res.json(product);
     } else {
-      res.status(404).json({ message: "Product not found" });
+      res.status(404);
+      throw new Error("Resource not found");
     }
   }),
 );
--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 connectDB();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 8000;
 
 const app = express();
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
